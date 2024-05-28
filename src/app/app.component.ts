a443aecf2457c4177bf63cec1786d0f35a433ba7
Andrii Andrusevych <aandrusevych@itsmartflex.com>
2024-05-28 10:27:23 +0300

    feature: added component to app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ComponentBookServiceListComponent} from "../component-book-service-list/component-book-service-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ComponentBookServiceListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
