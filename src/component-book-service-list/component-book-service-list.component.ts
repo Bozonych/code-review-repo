60ea13164becadc7aca478533353876c38b79ae9
Andrii Andrusevych <aandrusevych@itsmartflex.com>
2024-05-28 10:26:27 +0300

    feature: init component into src

---
+++ src/component-book-service-list/component-book-service-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-book-service-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-book-service-list.component.html',
+  styleUrl: './component-book-service-list.component.css'
+})
+export class ComponentBookServiceListComponent {
+
+}
